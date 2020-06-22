709ed37bd26b712c4770c3e7b439429fc8743d88
Matheus <matheus.marin@usp.br>
2020-06-22 18:56:40 -0300

    remove producer behaviour

--- src/main/java/controller/SpringController.java
+++ src/main/java/controller/SpringController.java
@@ -1,10 +1,7 @@
 package controller;
 
 import entities.UserVO;
-import infrastructure.rabbitmq.RabbitMQService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,12 +10,8 @@ import org.springframework.web.bind.annotation.RestController;
 @ComponentScan(basePackages = {"infrastructure.rabbitmq"})
 public class SpringController {
 
-    @Autowired
-    public RabbitMQService rabbitMQService;
-
     @PostMapping("/user")
     public String saveUser(@RequestBody UserVO payload) {
-        rabbitMQService.sendMessageToRabbit(payload.getIdUser(), payload.getName(), payload.getPassword());
-        return "user " + payload.getName() + " created in database!!";
+        return "hello";
     }
 }
