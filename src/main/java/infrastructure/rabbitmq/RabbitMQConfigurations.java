9f4815c3fd6b8de912dd99e1085c623f4c44a9bb
Matheus <matheus.marin@usp.br>
2020-07-05 16:01:36 -0300

    make it better

--- src/main/java/infrastructure/rabbitmq/RabbitMQConfigurations.java
+++ src/main/java/infrastructure/rabbitmq/RabbitMQConfigurations.java
@@ -4,12 +4,17 @@ import org.springframework.amqp.core.Binding;
 import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.rabbit.listener.adapter.MessageListenerAdapter;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.converter.MappingJackson2MessageConverter;
+
+import java.net.URI;
+import java.net.URISyntaxException;
 
 @Configuration
 public class RabbitMQConfigurations {
@@ -55,5 +60,22 @@ public class RabbitMQConfigurations {
         return new MessageListenerAdapter(listener, "listen");
     }
 
+    @Bean
+    public ConnectionFactory connectionFactory() {
+        final URI rabbitMqUrl;
+        try {
+            rabbitMqUrl = new URI("amqp://pbinmofj:5gvlRptD4d8gi-WCMZjfpH8GJ20aOnV0@buck.rmq.cloudamqp.com/pbinmofj");
+        } catch (URISyntaxException e) {
+            throw new RuntimeException(e);
+        }
 
+        final CachingConnectionFactory factory = new CachingConnectionFactory();
+        factory.setUsername(rabbitMqUrl.getUserInfo().split(":")[0]);
+        factory.setPassword(rabbitMqUrl.getUserInfo().split(":")[1]);
+        factory.setHost(rabbitMqUrl.getHost());
+        factory.setPort(rabbitMqUrl.getPort());
+        factory.setVirtualHost(rabbitMqUrl.getPath().substring(1));
+
+        return factory;
+    }
 }
